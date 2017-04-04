e0dfeb422febec6f9b584b28143867df75e896ef
Bartosz Witkowski <anselmos@users.noreply.github.com>
2017-04-04 23:45:46 +0200

    Pelican bootstrap theme (#5)
    Fixes #1
    
    * Adds initial pelican-bootstrap3 theme with needed plugin
    
    * Adds few possible changes to pelican for theme
    
    * Adds possible Display Categories for site.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,12 +3,35 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Anselmos'
-SITENAME = u'Example'
+SITENAME = u'Anselmos-Pelican-Blog Example'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Warsaw'
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['i18n_subsites']
+
+THEME = "themes/pelican-bootstrap3"
+# { PELICAN_BOOTSTRAP3_THEME
+JINJA_ENVIRONMENT = {
+	"extensions": ['jinja2.ext.i18n'],
+}
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+MENUITEMS = [['Menu-Item', "link"]]
+PYGMENTS_STYLE = 'vim' # coloring of code blocks - style with pygments
+BOOTSTRAP_NAVBAR_INVERSE = True ## for inversing colors at navbar
+GITHUB_USER = 'anselmos'  ## just put your github username here.
+GITHUB_SKIP_FORK = True
+ABOUT_ME = """ Developer. Mainly found at backyard of - pick yourself: (sorted by name) : android | docker | java | linux | python """
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+
+# PELICAN_BOOTSTRAP3_THEME }
+
+
+STATIC_PATHS = ['images']
 
 DEFAULT_LANG = u'E\x08'
 
@@ -26,8 +49,9 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', '#'),
+          ('twitter', '#'),
+          ('linkedin', '#'),)
 
 DEFAULT_PAGINATION = 6
 
